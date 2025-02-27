54f1153b6806324f29bb928c1d3135ace453be31
MegaEmme <mikmusco@gmail.com>
2025-02-27 09:23:25 +0100

    fixed: es 2.5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -59,5 +59,5 @@ const isFabioPresent = teachers.indexOf('Fabio') > -1;
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.join();
+const teachersString = teachers.toString();
 console.log(teachersString);
